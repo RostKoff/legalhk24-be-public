de6d41e1c087b12afbb450bf1f07d66bd1b269aa
RostKoff <r.kovalenko.it@gmail.com>
2024-04-29 19:10:29 +0200

    Project files added to public repo

---
+++ fourLegal/src/test/java/com/example/fourLegal/FourLegalApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.fourLegal;
+
+import com.example.fourLegal.contract.ChatResponseDto;
+import com.example.fourLegal.contract.PromptDto;
+import com.example.fourLegal.helpers.PromptHolder;
+import com.example.fourLegal.helpers.TemplateCheckHelper;
+import com.example.fourLegal.services.DocumentAIService;
+import com.example.fourLegal.services.OpenAIService;
+import com.example.fourLegal.services.StorageService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class FourLegalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
